67f3088565f388379e05b9942dc30c89df6352da
Janus <ysangkok@gmail.com>
2016-07-05 11:08:39 +0200

    now finally works

--- app/Main.hs
+++ app/Main.hs
@@ -26,36 +26,36 @@ import Control.Monad.IO.Class
 import Data.Monoid
 import Data.String
 
-main= simpleWebApp 8081 $ local $ buttons <|> linksample
+main= simpleWebApp 8081 $ local $  buttons  <|> linksample
+  where
+  linksample= do
+      r <- render $ br ++> wlink "Hi!" (toElem "This link say Hi!")`fire` OnClick
+      render $ rawHtml . b  $ " returns "++ r
 
-linksample= do
-      r <- br ++> wlink "Hi!" (toElem "This link say Hi!")`fire` OnClick 
-      rawHtml . b  $ " returns "++ r
+  buttons= do
+       render . rawHtml $ p "Different input elements:"
+       radio
+           **> br ++> br
+           ++> mselect
+           <++ br
 
-buttons= p "Different input elements:" ++> checkButton
-                                       **> br ++> br
-                                       ++> radio
-                                       **> br ++> br
-                                       ++> select
-                                       <++ br
-    where
-    checkButton=do
-       rs <- getCheckBoxes(
+  checkButton=do
+       rs <- render $ getCheckBoxes(
                        ((setCheckBox False "Red"    <++ b "red")   `fire` OnClick)
                     <> ((setCheckBox False "Green"  <++ b "green") `fire` OnClick)
                     <> ((setCheckBox False "blue"   <++ b "blue")  `fire` OnClick))
-       wraw $ fromString " returns: " <> b (show rs)
+       render $ wraw $ fromString " returns: " <> b (show rs)
 
-    radio= do
-       r <- getRadio [fromString v ++> setRadioActive v | v <- ["red","green","blue"]]
+  radio= do
+         r <- render $ getRadio [fromString v ++> setRadioActive v | v <- ["red","green","blue"]]
 
-       wraw $ fromString " returns: " <> b ( show r )
+         render $ wraw $ fromString " returns: " <> b ( show r )
 
-    select= do
-       r <- getSelect (   setOption "red"   (fromString "red")  
+mselect= do
+       r <- render $ getSelect (   setOption "red"   (fromString "red")
 
                       <|> setOption "green" (fromString "green")
                       <|> setOption "blue"  (fromString "blue"))
               `fire` OnClick
 
-       wraw $ fromString " returns: " <> b ( show r )
+       render $ wraw $ fromString " returns: " <> b ( show r )
--- copy_and_start_server.sh
+++ copy_and_start_server.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
+set -e
+set -x
+stack setup
 stack build
+stack setup --compiler=ghcjs-0.2.0_ghc-7.10.3
 stack build --compiler=ghcjs-0.2.0_ghc-7.10.3
 mkdir -p static/out.jsexe
 cp -vr .stack-work/install/x86_64-linux/lts-6.5/ghcjs-0.2.0_ghc-7.10.3/bin/transient-test-exe.jsexe/. static/out.jsexe/
